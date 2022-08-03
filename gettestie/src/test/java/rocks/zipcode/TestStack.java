b8297ce3fcab1f2fb20e1ccea3d0cc646e42f778
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-08-03 13:59:50 -0400

    stacks

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,5 +1,8 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.Stack;
 
@@ -22,7 +25,123 @@ public class TestStack {
 
     // Make a bigger test exercising more Stack methods.....
 
+    @Test
+    public void pushTest() {
+        // Given
+        Stack<Person> stack = new Stack<>();
+        Person person = new Person("Lalisa", 1997);
+        Integer expected = 1;
 
+        // When
+        stack.push(person);
+        Integer actual = stack.size();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void peekCheckValueTest() {
+        // Given
+        Stack<String> stack = new Stack<>();
+        String orange = "orange";
+        String cherry = "cherry";
+
+        // When
+        stack.push(orange);
+        stack.push(cherry); //this is the top of the stack...
+        String actual = stack.peek();
+
+        // Then
+        Assert.assertEquals(cherry, actual);
+    }
+
+    @Test
+    public void peekCheckSizeTest() {
+        // Given
+        Stack<String> stack = new Stack<>();
+        String orange = "orange";
+        String cherry = "cherry";
+        Integer expected = 2;
+
+        // When
+        stack.push(orange);
+        stack.push(cherry);
+        stack.peek();
+        Integer actual = stack.size();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void popCheckValueTest() {
+        // Given
+        Stack<String> stack = new Stack<>();
+        String apple = "apple";
+        String orange = "orange";
+        String cherry = "cherry";
+
+        // When
+        stack.push(orange);
+        stack.push(cherry);
+        stack.push(apple);
+        String actual = stack.pop(); //pop removes it
+
+        // Then
+        Assert.assertEquals(apple, actual);
+    }
+
+    @Test
+    public void popCheckSizeTest() {
+        // Given
+        Stack<String> stack = new Stack<>();
+        String apple = "apple";
+        String orange = "orange";
+        String cherry = "cherry";
+        Integer expected = 1;
+
+        // When
+        stack.push(orange);
+        stack.push(cherry);
+        stack.push(apple);
+        stack.pop();
+        stack.pop();
+        Integer actual = stack.size();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isEmptyTrueTest() {
+        // Given
+        Stack<Integer> stack = new Stack<>();
+        Boolean expected = true;
+
+        // When
+        Boolean actual = stack.empty();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isEmptyFalseTest() {
+        // Given
+        Stack<Integer> stack = new Stack<>();
+        Integer one = 1;
+        Integer ten = 10;
+        Boolean expected = false;
+
+        // When
+        stack.push(one);
+        stack.push(ten);
+        Boolean actual = stack.empty();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 
 
 
